dc78a013eac1ad12cf6d16c71edff114174612b5
Nguyễn Ngọc Truyện <truyen027@gmail.com>
2023-05-03 10:47:40 +0700

    add Resful APT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
-const Account = require('./models/Account.js');
+const accountRouter = require('./routers/Acount.js');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -9,41 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-    res.json('Đã kết nối thành công');
-})
-
-app.post('/register', (req, res, next) => {
-    Account.create({
-        username: req.body.username,
-        password: req.body.password
-    })
-        .then(() => {
-            res.json('Tạo tài khoản thành công');
-        })
-        .catch(() => {
-            res.json('Tạo tài khoản thất bại');
-        })
-})
-
-app.post('/login', (req, res, next) => {
-    Account.findOne(
-        {
-            username: req.body.username,
-            password: req.body.password
-        }
-    )
-        .then(data => {
-            if (data)
-                res.json('Đăng nhập thành công');
-            else
-                res.json('Đăng nhập thất bại');
-        })
-        .catch(() => {
-            res.status(500).json('Server đang bảo trì')
-        })
-})
-
+app.use('/api/account', accountRouter)
 
 
 app.listen(3000)
